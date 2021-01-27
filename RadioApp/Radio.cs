7851379a8b0bcc97cf25dd11ae4d2a2f0af255dd
jkchuong <jkchuong@gmail.com>
2021-01-27 17:51:38 +0000

    Working Radio Interface

---
+++ RadioApp/Radio.cs
@@ -0,0 +1,49 @@
+﻿namespace RadioApp
+{
+    public class Radio
+    {
+        public Radio()
+        {
+
+        }
+
+        private int _channel = 1;
+        private bool _on = false;
+
+        public int Channel 
+        { 
+            get => _channel;
+            set
+            {
+                if ((value == 1 || value == 2 || value == 3 || value == 4) && _on)
+                {
+                    _channel = value;
+                }
+            }
+        }
+
+        public bool On { get; set; }
+
+        public string Play()
+        {
+            if (_on)
+            {
+                return $"Playing channel {_channel}";
+            }
+            return "Radio is off";
+        }
+
+        public void TurnOff()
+        {
+            _on = false;
+        }
+        
+        public void TurnOn()
+        {
+            _on = true;
+        }
+
+    }
+    // implement a class Radio that corresponds to the Class diagram 
+    //   and specification in the Radio_Mini_Project document
+}
---
+++ RadioInterface/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using RadioApp;
+
+namespace RadioInterface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Radio _radio = new Radio();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Channel_Click(object sender, RoutedEventArgs e)
+        {
+            string channel = (sender as Button).Content.ToString();
+            int channelInt = Int16.Parse(channel);
+
+            _radio.Channel = channelInt;
+
+            Channel_Display.Text = _radio.Play();
+        }
+
+        private void Swtich_Click(object sender, RoutedEventArgs e)
+        {
+            _radio.On = !_radio.On;
+            
+            if (_radio.On)
+            {
+                Status_Display.Text = "ON";
+                Status_Display.Background = Brushes.Green;
+            }
+            else
+            {
+                Status_Display.Text = "OFF";
+                Status_Display.Background = Brushes.Red;
+            }
+        }
+    }
+}
